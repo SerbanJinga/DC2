15b529a1972123e1271d689aed9448ab21f7c5af
SerbanJinga <33983121+SerbanJinga@users.noreply.github.com>
2023-04-28 16:40:39 +0200

    Dataframe for Burglaries in the Barnet borough is now defined
    Extracted all of the data for London's Barnet borough, using the LSOA codes for the specific regions.

--- main.py
+++ main.py
@@ -10,4 +10,18 @@ def get_dataset(path) -> pd.DataFrame:
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     df = get_dataset("./data/2023-02-metropolitan-street.csv")
-    df_burglaries = df[df["Crime type"] == "Burglary"]
+    df_burglaries = df[df["Crime type"] == "Burglary"].copy()
+    # print(df_burglaries["LSOA code"].unique())
+    # print(df_burglaries["LSOA name"].unique())
+    df_burglaries_Barnet = pd.DataFrame(columns=df_burglaries.columns)
+    for index in range(len(df_burglaries)):
+        # print(df_burglaries[index])
+        x = df_burglaries.iloc[index]["LSOA code"]
+        if isinstance(x, float):
+            continue
+        else:
+            if int(x[2:]) >= 1000001 and int(x[2:]) <= 1004747:
+                df_burglaries_Barnet.loc[index] = (df_burglaries.iloc[index])
+
+    df_burglaries_Barnet.drop(columns=["Context"], inplace=True)
+    print(df_burglaries_Barnet.head(20))
