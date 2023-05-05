d3f36abe342526fcc79183e27ee97cb3110bfe74
SerbanJinga <33983121+SerbanJinga@users.noreply.github.com>
2023-05-05 18:16:29 +0200

    Added all of the data from april 2020 to march 2023. We now have 15000 entries. Filtered data on the specific LSOA of the Barnett borough of London and crime type of burglary.

--- main.py
+++ main.py
@@ -27,7 +27,7 @@ def parse_dataframe(path):
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     df_burglaries_Barnet_february = parse_dataframe("./data/2023-02-metropolitan-street.csv")
-    df_burglaries_Barnet_march = parse_dataframe("./data/2023-02-metropolitan-street.csv")
+    df_burglaries_Barnet_march = parse_dataframe("./data/2023-03-metropolitan-street.csv")
     january = parse_dataframe("./data/2023-01-metropolitan-street.csv")
     december = parse_dataframe("./data/2022-12-metropolitan-street.csv")
     november = parse_dataframe("./data/2022-11-metropolitan-street.csv")
@@ -42,5 +42,28 @@ if __name__ == '__main__':
     february = parse_dataframe("./data/2022-02-metropolitan-street.csv")
     january = parse_dataframe("./data/2022-01-metropolitan-street.csv")
 
-    df_all = pd.concat([january, february, march, april, may, june, july, august, september, october, november, december, january, df_burglaries_Barnet_february, df_burglaries_Barnet_march])
+    january2021 = parse_dataframe("./data/2021-01-metropolitan-street.csv")
+    february2021 = parse_dataframe("./data/2021-02-metropolitan-street.csv")
+    march2021 = parse_dataframe("./data/2021-03-metropolitan-street.csv")
+    april2021 = parse_dataframe("./data/2021-04-metropolitan-street.csv")
+    may2021 = parse_dataframe("./data/2021-05-metropolitan-street.csv")
+    june2021 = parse_dataframe("./data/2021-06-metropolitan-street.csv")
+    july2021 = parse_dataframe("./data/2021-07-metropolitan-street.csv")
+    august2021 = parse_dataframe("./data/2021-08-metropolitan-street.csv")
+    september2021 = parse_dataframe("./data/2021-09-metropolitan-street.csv")
+    october2021 = parse_dataframe("./data/2021-10-metropolitan-street.csv")
+    november2021 = parse_dataframe("./data/2021-11-metropolitan-street.csv")
+    december2021 = parse_dataframe("./data/2021-12-metropolitan-street.csv")
+    january2020 = parse_dataframe("./data/2020-04-metropolitan-street.csv")
+    february2020 = parse_dataframe("./data/2020-05-metropolitan-street.csv")
+    march2020 = parse_dataframe("./data/2020-06-metropolitan-street.csv")
+    april2020 = parse_dataframe("./data/2020-07-metropolitan-street.csv")
+    may2020 = parse_dataframe("./data/2020-08-metropolitan-street.csv")
+    june2020 = parse_dataframe("./data/2020-09-metropolitan-street.csv")
+    july2020 = parse_dataframe("./data/2020-10-metropolitan-street.csv")
+    august2020 = parse_dataframe("./data/2020-11-metropolitan-street.csv")
+    september2020 = parse_dataframe("./data/2020-12-metropolitan-street.csv")
+
+
+    df_all = pd.concat([september2020, august2020, july2020, june2020, may2020, april2020, march2020, february2020, january2020, december2021, november2021, october2021, september2021, august2021, july2021, june2021, may2021, april2021, march2021, february2021, january2021 ,  january, february, march, april, may, june, july, august, september, october, november, december, january, df_burglaries_Barnet_february, df_burglaries_Barnet_march])
     df_all.to_csv("./data/january_2022_to_march_2023.csv")
--- test.py
+++ test.py
@@ -2,4 +2,4 @@ import pandas as pd
 
 if __name__ == "__main__":
     df = pd.read_csv("./data/january_2022_to_march_2023.csv")
-    print(df.head(20))
+    print(len(df)) # len of the dataset is now 150000
