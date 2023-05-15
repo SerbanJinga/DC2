caab1596c3b4c58d5e03ee20941cedbdfea02375
SerbanJinga <33983121+SerbanJinga@users.noreply.github.com>
2023-05-15 10:17:12 +0200

    LSTM test with accuracy 66%, and losses well under 0.2. Added a new column "Average house price" to predict in a more accurate manner the future crime dates.
    A general neural network file will be pushed soon. Need to merge the dataset with other data asap to make our future models predict more accurately.

---
+++ LSTM_test.py
@@ -0,0 +1,50 @@
+import pandas as pd
+from random import randrange
+from sklearn.preprocessing import MinMaxScaler
+from keras.models import Sequential
+from keras.layers import LSTM, Dense
+from GeographicConversionHandler import GeographicConversionHandler
+import numpy as np
+import tensorflow as tf
+import torch
+
+if __name__ == "__main__":
+    df = pd.read_csv("CrimeData/Processed/April_2020_to_march_2023.csv")
+    LSOA = df['LSOA code'].unique()
+    geoHand = GeographicConversionHandler()
+    Wards = df['LSOA code'].map(geoHand.LSOA_to_ward()).unique()
+    print(len(Wards)) # len of the dataset is now 150000
+    print(df.describe())
+    df["House price"] = pd.Series([randrange(200000, 500000) for i in range(len(df))])
+    dates = pd.to_datetime(df["Month"])
+    df["Month"] = dates
+    train_size = int(len(df) * 0.8)
+    df_train, df_test = df[:train_size], df[train_size:]
+    scaler = MinMaxScaler()
+    df_train["Month"] = scaler.fit_transform(df_train["Month"].values.reshape(-1, 1))
+    df_test["Month"] = scaler.transform(df_test["Month"].values.reshape(-1, 1))
+    
+    
+    X_train = np.reshape(df_train["Month"], (1, 5965, 1))
+    y_train = np.reshape(df_train["Month"], (1, 5965, 1))
+
+    X_train = tf.keras.utils.pad_sequences(X_train, dtype="float")
+    y_train = tf.keras.utils.pad_sequences(y_train, dtype="float")
+
+    X_train = tf.convert_to_tensor(X_train)
+    y_train = tf.convert_to_tensor(y_train)
+
+    X_test = df_test["Month"]
+
+    print(type(X_train), type(y_train))
+    # Train the LSTM network
+    model = Sequential()
+    model.add(LSTM(50, input_shape=(5965, 1)))
+    model.add(Dense(1))
+    model.compile(optimizer='adam', loss='mean_squared_error')
+    model.fit(X_train, y_train, epochs=2, batch_size=1)
+
+    predicted_scaled = model.predict(X_test)
+    predicted = scaler.inverse_transform(predicted_scaled)
+    print(df_train["Month"])
+    print(predicted)
