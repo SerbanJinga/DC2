907853b016659e16907f2980f9774091c8d6f198
ozndn <ozndnz77@gmail.com>
2023-05-30 19:20:44 +0200

    The output of the neural network is more presentable. Right now we have probabilities of the crime likelihood which is multiplied by police officer count (100) and added into a dataframe. Additionaly a bar chart is plotted as a visualization. (Serban and Deniz)

--- LSTM/NeuralNetworksPredict.py
+++ LSTM/NeuralNetworksPredict.py
@@ -2,7 +2,7 @@ import numpy as np
 import pandas as pd
 import tensorflow as tf
 from keras.optimizers import Adam
-
+import seaborn as sns
 from Net import getNet
 
 from LinearRegressionAnalysis import create_convolutional_data
@@ -33,13 +33,15 @@ df = pd.read_csv(ward_file)
 X, y, dates = create_convolutional_data(data=df, historic_data_series=convolutional_series, month_power=month_power, normalize=True)
 
 Wards = df[df.columns[0]].values
-X_train, X_test, y_train, y_test = train_test_split(X.T, y.T, test_size=0.5, shuffle=False)
+X_train, X_test, y_train, y_test = train_test_split(X.T, y.T, test_size=0.4, shuffle=False)
 
 normalization_mean = X_train.mean() if turn_into_z_score else 0
 normalization_std = X_train.std() if turn_into_z_score else 1
 
 def normalize_into_Zscore(a):
     return (a-normalization_mean)/normalization_std
+def denormalize(a):
+    return (a*normalization_std)* np.sqrt(len(y_train[0]))
 
 X_train, X_test, y_train, y_test = normalize_into_Zscore(X_train), normalize_into_Zscore(X_test), normalize_into_Zscore(y_train), normalize_into_Zscore(y_test)
 
@@ -81,9 +83,9 @@ def run_model_battery(number_of_times):
     for n in range(number_of_times):
         print(f'Working with model number {n + 1}')
         history = model.fit(X_train, y_train, epochs=epochs, batch_size=batch_size, validation_data=(X_test, y_test), verbose=0)
+
         rmse_loss[n] = np.sqrt(np.array(history.history['loss'], dtype=np.float64))
         rmse_val_loss[n] = np.sqrt(np.array(history.history['val_loss'], dtype=np.float64))
-
     loss_matrix = np.array(rmse_loss)
     val_loss_matrix = np.array(rmse_val_loss)
 
@@ -105,3 +107,43 @@ def run_model_battery(number_of_times):
 if __name__ == '__main__':
     run_model_battery(3)
     #run_single_model()
+    current_year = 2023
+    # current_month = 1
+    # dict_months = {
+    #     1: "January",
+    #     2: "February",
+    #     3: "March",
+    #     4: "April",
+    #     5: "May",
+    #     6: "June",
+    #     7: "July",
+    #     8: "August",
+    #     9: "September",
+    #     10: "October",
+    #     11: "November",
+    #     12: "December"}
+    predictions = model.predict(X_test)
+    predictions_unscaled = ((predictions * normalization_std) + normalization_mean)
+    # ward_prediction = pd.DataFrame({'Wards': Wards, 'Prediction_count': []})
+    # print(ward_prediction)
+    ward_dict = {}
+    for i, ward in enumerate(Wards):
+        ward_dict[ward] = predictions_unscaled[:, i][0]*100
+        print(f'Ward: {ward}, Predicted Crime Count: {predictions_unscaled[:, i]}')
+    ward_predicitions = pd.DataFrame(ward_dict.items())
+    ward_predicitions.rename(columns = {0 : "Ward", 1: "Crime Likelihood"}, inplace = True)
+    ward_predicitions.set_index("Ward")
+
+    plt.figure(figsize=(10, 6))
+    ax = sns.barplot(x='Ward',
+                     y='Crime Likelihood',
+                     data=ward_predicitions)
+    ax.set_xticklabels(ax.get_xticklabels(), rotation=45, ha='right')
+    plt.xlabel('Ward')
+    plt.ylabel('Crime Likelihood')
+    plt.title('Crime Likelihood by Ward')
+    plt.tight_layout()
+    plt.show()
+    print(ward_dict)
+    print(ward_predicitions)
+    print(ward_predicitions['Crime Likelihood'].sum())
