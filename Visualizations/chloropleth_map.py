c45abebdcb0ca1e769617e1ea0af0a0abf5b9d2f
ozndn <ozndnz77@gmail.com>
2023-06-08 13:55:19 +0200

    Made a chloropleth map to make a comparison between what we have predicted and what is the actual crime count on that month

--- Visualizations/chloropleth_map.py
+++ Visualizations/chloropleth_map.py
@@ -4,28 +4,47 @@ import csv
 import plotly.express as px
 import json
 
-dataset = pd.read_csv('../CrimeData/Processed/january_2022_to_march_2023.csv')
-with open('../GeographicalData/Raw/lsoa.geojson', 'r') as geojsonfile:
+dataset = pd.read_csv('../CrimeData/Processed/December_2012_to_march_2023.csv')
+print(dataset.columns)
+with open('../GeographicalData/Raw/GeoWards.geojson', 'r') as geojsonfile:
     geojson = json.load(geojsonfile)
 
-dataset.drop(dataset[dataset['LSOA code'].isnull()].index, inplace=True)
+dataset.drop(dataset[dataset['Ward'].isnull()].index, inplace=True)
+
+dataset['Month'] = pd.to_datetime(dataset['Month'])
+next_month = dataset[dataset['Month'].dt.strftime('%Y-%m') == '2018-06']
+print(next_month[['Month', 'Ward']])
+
+grouped_data = next_month.groupby(['Ward'])['Crime ID'].agg('count')
+grouped = pd.DataFrame(grouped_data).reset_index()
+grouped.columns = ['Ward', 'Count']
+grouped['percentage'] = (grouped['Count']/grouped['Count'].sum())*100
 
 
-grouped_data = dataset.groupby(['LSOA code'])['Crime ID'].agg('count')
-# print(grouped_data)
-dictionary = grouped_data.to_dict()
-# print(dictionary)
+def largest_remainder_round(df, column_name):
+    total = df[column_name].sum()
+    df['rounded'] = df[column_name] // 1  # Floor division
+    remainder = int(total - df['rounded'].sum())  # Convert remainder to integer
+    rounded_values = df['rounded'].values.tolist()
+    sorted_indices = sorted(range(len(rounded_values)), key=lambda i: -(rounded_values[i] % 1))
+
+    for i in range(remainder):
+        rounded_values[sorted_indices[i]] += 1
+    print(df)
+    df['rounded'] = rounded_values
+
+    return df
+grouped= largest_remainder_round(grouped,'percentage')
+
+print(grouped['rounded'].sum())
 
 
 
-dataset['count'] = dataset['LSOA code'].apply(lambda x: dictionary[x])
-# print(dataset)
-
-fig = px.choropleth_mapbox(dataset, geojson=geojson, color='count',
-                           locations="LSOA code", featureidkey="properties.LSOA11CD",
+fig = px.choropleth_mapbox(grouped, geojson=geojson, color='rounded',
+                           locations="Ward", featureidkey="properties.WD13NM",
                            center={"lat": 51.509865, "lon": -0.118092},
                            mapbox_style="carto-positron", zoom=9,
-                           labels= ['LSOA name','count'])
+                           labels= ['Ward','count'])
 fig.show()
 
 
